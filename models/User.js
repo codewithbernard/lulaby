65dca1740a2c2b88a5836f12fa74e1ce1c867969
Bernard Bado <bado.bernard94@gmail.com>
2017-11-05 17:40:03 +0100

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// No need to export mongoose code. Mongoose can be confused when trying
+// to create multiple models with same name
+const userSchema = new Schema({
+  spotifyId: String
+});
+
+mongoose.model('users', userSchema);
---
+++ routes/authRoutes.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get('/auth/spotify', passport.authenticate(
+    'spotify',
+    {scope: ['user-read-email', 'user-read-private']}
+  ));
+
+  app.get('/auth/spotify/callback', passport.authenticate('spotify'), (req, res) => {
+    res.redirect('/auth/currentUser');
+  });
+
+  app.get('/auth/currentUser', (req, res) => {
+    res.send({
+      spotifyId: req.user.spotifyId
+    });
+  })
+}
