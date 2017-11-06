b0ae4d50f11ac87b229882b5ba6d303499df1f56
Bernard Bado <bado.bernard94@gmail.com>
2017-11-06 19:06:28 +0100

    Returning users with at least 3 common artists

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,19 @@ const _ = require('lodash');
 
 module.exports = app => {
   app.get('/users/find', async (req, res) => {
+    let databaseMatchCount = 2;
+    let totalMatchCount = 3;
+    
     const { favoriteArtists } = req.user;
-    console.log(favoriteArtists);
     // Get users from database which has in his list top 2 artist
-    const users = await User.find({ favoriteArtists: { "$all" : _.slice(favoriteArtists, 0, 2)} });
-    res.send(users);
-    // Go through this user and find the 3rd match.
+    const users = await User.find({spotifyId: { $ne: req.user.spotifyId }, favoriteArtists: { "$all" : _.slice(favoriteArtists, 0, databaseMatchCount)} });
+
+    // Go through this users and find the 3rd match.
+    const matchedUsers = _.filter(users, user => {
+      return _.intersection(favoriteArtists, user.favoriteArtists).length >= totalMatchCount;
+    });
 
     // Return list of users with 3 matches
+    res.send(matchedUsers);
   });
 }
