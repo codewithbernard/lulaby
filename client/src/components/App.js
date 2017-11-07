3f83fe6192020993e90a44499b2d7a1c78c9d87e
bernard <bado.bernard94@gmail.com>
2017-11-07 11:11:06 +0100

    Deleting unnecessary files and adding landing files

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hejlo
+      </div>
+    );
+  }
+}
+
+export default App;
