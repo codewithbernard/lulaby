80a069c25fb5202b329d31d78803340e3a96bd38
Bernard Bado <bado.bernard94@gmail.com>
2017-11-07 19:49:09 +0100

    Some texts added

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -31,7 +31,7 @@ class Landing extends Component {
           <div className="section no-pad-bot">
             <div className="container">
             <br/>
-              <h1 className="header center teal-text text-lighten-2">Lullaby</h1>
+              <h1 className="header center">Lullaby</h1>
               <div className="row center">
                 <h4 className="header col s12 light">Find people with similar taste in music like YOU</h4>
               </div>
@@ -53,27 +53,27 @@ class Landing extends Component {
               <div className="col s12 m4">
                 <div className="icon-block">
                   <h2 className="center brown-text"><i className="material-icons">flash_on</i></h2>
-                  <h5 className="center">Speeds up development</h5>
+                  <h5 className="center">Use your Spotify account</h5>
 
-                  <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                  <p className="light">No boring and long registration needed. Just use your already created Spotify account. Lullaby will analyze your favorite artists and find people with similar taste in music. It is simple as that.</p>
                 </div>
               </div>
 
               <div className="col s12 m4">
                 <div className="icon-block">
                   <h2 className="center brown-text"><i className="material-icons">group</i></h2>
-                  <h5 className="center">User Experience Focused</h5>
+                  <h5 className="center">Find new friends</h5>
 
-                  <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                  <p className="light">By utilizing Spotify tools, we are able to identify users listening habits and their taste in music. Comparing these units, we are able to find people around you who listen same music as you. How cool is that?</p>
                 </div>
               </div>
 
               <div className="col s12 m4">
                 <div className="icon-block">
                   <h2 className="center brown-text"><i className="material-icons">settings</i></h2>
-                  <h5 className="center">Easy to work with</h5>
+                  <h5 className="center">Then it's up to you</h5>
 
-                  <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                  <p className="light">What you will do next is entirely up to you. You can find new friends, partner for a concert or festival. What the heck, maybe it will be you future wife/husband. Who knows? Don't waste your time and let's get started.</p>
                 </div>
               </div>
             </div>
@@ -86,7 +86,7 @@ class Landing extends Component {
           <div className="section no-pad-bot">
             <div className="container">
               <div className="row center">
-                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <h5 className="header col s12 light">If music be the food of love play on</h5>
               </div>
             </div>
           </div>
@@ -99,8 +99,8 @@ class Landing extends Component {
             <div className="row">
               <div className="col s12 center">
                 <h3><i className="mdi-content-send brown-text"></i></h3>
-                <h4>Contact Us</h4>
-                <p className="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+                <h4>Still hesitate?</h4>
+                <p className="left-align light">It is simple, easy and fast. Just login using your Spotify account and find friends with same interest in music like you. It takes only 30 seconds. If you will not be satisfied you can delete your account at any time. No ads, no spams and completelz free. Just you and the people you will probably like. So don't waste your time and let's get started. It is only one click away.</p>
               </div>
             </div>
 
@@ -112,7 +112,7 @@ class Landing extends Component {
           <div className="section no-pad-bot">
             <div className="container">
               <div className="row center">
-                <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                <h5 className="header col s12 light">Without music, life would be a mistake</h5>
               </div>
             </div>
           </div>
