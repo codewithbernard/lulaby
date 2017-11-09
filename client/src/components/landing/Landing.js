3abdf12ae6abf3cd32d49b09c5021dba30b9ca45
bernard <bado.bernard94@gmail.com>
2017-11-09 13:10:45 +0100

    Finishing landing page

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -106,8 +106,6 @@ class Landing extends Component {
 
           </div>
         </div>
-
-
         <div className="parallax-container valign-wrapper">
           <div className="section no-pad-bot">
             <div className="container">
@@ -123,34 +121,24 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col l6 s12">
-                <h5 className="white-text">Company Bio</h5>
-                <p className="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
+                <h5 className="white-text">About Lullaby</h5>
+                <p className="grey-text text-lighten-4">I created this app as a part of learning React. Any ideas and improvements are more than welcome.</p>
               </div>
               <div className="col l3 s12">
-                <h5 className="white-text">Settings</h5>
-                <ul>
-                  <li><a className="white-text" href="#!">Link 1</a></li>
-                  <li><a className="white-text" href="#!">Link 2</a></li>
-                  <li><a className="white-text" href="#!">Link 3</a></li>
-                  <li><a className="white-text" href="#!">Link 4</a></li>
-                </ul>
               </div>
               <div className="col l3 s12">
                 <h5 className="white-text">Connect</h5>
                 <ul>
-                  <li><a className="white-text" href="#!">Link 1</a></li>
-                  <li><a className="white-text" href="#!">Link 2</a></li>
-                  <li><a className="white-text" href="#!">Link 3</a></li>
-                  <li><a className="white-text" href="#!">Link 4</a></li>
+                  <li><a className="white-text" href="https://www.linkedin.com/in/bernard-bado-416a8787/">LinkedIn</a></li>
+                  <li><a className="white-text" href="https://www.facebook.com/bernard.bado">Facebook</a></li>
+                  <li><a className="white-text" href="mailto:bado.bernard94@gmail.com">Mail</a></li>
                 </ul>
               </div>
             </div>
           </div>
           <div className="footer-copyright">
             <div className="container">
-            Made by <a className="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+            © 2017 Lullaby. All rights reserved. Design by <a className="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
             </div>
           </div>
         </footer>
