cd10b15d4b3ddecd0c821a468568f7b818352c2f
Bernard Bado <bado.bernard94@gmail.com>
2018-01-30 13:38:11 +0100

    Improving Sidenav

--- client/src/components/explore/Sidenav.js
+++ client/src/components/explore/Sidenav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavLink from '../common/NavLink';
 
 import './Sidenav.css';
 
@@ -8,8 +9,8 @@ class Sidenav extends Component {
       <ul id="slide-out" className="side-nav fixed">
         <li className="logo"><a id="logo-container" href="/explore#" class="brand-logo">
         <object id="front-page-logo" type="image/svg+xml" data="logo.svg">Your browser does not support SVG</object></a></li>
-        <li><a href="#!"><i class="material-icons">whatshot</i> Look around</a></li>
-        <li><a href="#!"><i class="material-icons">person</i> My profile</a></li>
+        <NavLink className="waves-effect waves-teal" to="/explore/users"><i class="material-icons">whatshot</i> Look around</NavLink>
+        <NavLink className="waves-effect waves-teal" to="/explore/me"><i class="material-icons">person</i> My profile</NavLink>
       </ul>,
       <a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
     ];
