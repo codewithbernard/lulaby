0c2d2eac84ace6e9926e6c4325a071f68c8b5a7e
Bernard Bado <bado.bernard94@gmail.com>
2018-01-31 14:39:11 +0100

    Hiding cloudinary secrets

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -15,9 +15,9 @@ export const updateUser = (user, values) => async dispatch => {
     // Upload iamge to cloudinare if new image was chosen
     if (values.image.item) {
       let data = new FormData();
-      data.append('upload_preset', 'snwikmyi');
+      data.append('upload_preset', process.env.REACT_APP_UPLOAD_PRESET);
       data.append('file', values.image.item(0))
-      let res = await axios.post('https://api.cloudinary.com/v1_1/dypjti8qj/image/upload', data);
+      let res = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_ID}/image/upload`, data);
       values.image = res.data.url;
     }
     axios.put(`/api/users/${user}`, values);
