fb6342f50c913088a0e726a7e7a9ebbef99f103a
Bernard Bado <bado.bernard94@gmail.com>
2018-02-02 17:28:55 +0100

    Disabling button if user already requested

--- client/src/components/explore/UserListItem.js
+++ client/src/components/explore/UserListItem.js
@@ -20,7 +20,7 @@ class UserListItem extends Component {
           <div className="card-image">
             <img className="user-list-item-card-image" src={this.props.user.image} alt=""/>
             <span className="card-title">{this.props.user.spotifyId}, {this.props.user.age}</span>
-            <button onClick={this.handleButtonClick.bind(this)} className={`btn-floating halfway-fab waves-effect waves-teal teal lighten-2 ${this.isFriendRequested() ? "disabled" : null}`}><i className="material-icons">message</i></button>
+            <button onClick={this.handleButtonClick.bind(this)} className={`btn-floating halfway-fab waves-effect waves-teal teal lighten-2 ${this.isFriendRequested() ? "disabled" : null}`}><i className="material-icons">person_add</i></button>
           </div>
           <div className="card-content">
             <p>{this.props.user.about}</p>
