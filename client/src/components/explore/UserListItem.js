09942143ab0ae5302a0d14f93b0960acc332878f
Bernard Bado <bado.bernard94@gmail.com>
2018-02-02 17:40:52 +0100

    Not displaying user if he already sent request

--- client/src/components/explore/UserListItem.js
+++ client/src/components/explore/UserListItem.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import * as actions from '../../actions';
 
 import './UserListItem.css';
 
 class UserListItem extends Component {
-  handleButtonClick() {
-    axios.get(`/api/notify/${this.props.user.spotifyId}`);
+  async handleButtonClick() {
+    await axios.get(`/api/notify/${this.props.user.spotifyId}`);
+    this.props.fetchUsers();
   }
 
+  // Check if friend request was already sent
   isFriendRequested() {
     return this.props.user.friendRequests.filter(user => user.spotifyId === this.props.auth.spotifyId).length > 0;
   }
 
+  // Check if user already sent friend request to you
+  friendRequestedYou() {
+    return this.props.auth.friendRequests.filter(user => user.spotifyId === this.props.user.spotifyId).length > 0;
+  }
+
   render() {
-    return(
-      <div className="col s12 m6 l6 xl4">
-        <div className="user-list-item-card card">
-          <div className="card-image">
-            <img className="user-list-item-card-image" src={this.props.user.image} alt=""/>
-            <span className="card-title">{this.props.user.spotifyId}, {this.props.user.age}</span>
-            <button onClick={this.handleButtonClick.bind(this)} className={`btn-floating halfway-fab waves-effect waves-teal teal lighten-2 ${this.isFriendRequested() ? "disabled" : null}`}><i className="material-icons">person_add</i></button>
-          </div>
-          <div className="card-content">
-            <p>{this.props.user.about}</p>
+    if (!this.friendRequestedYou()) {
+      return(
+        <div className="col s12 m6 l6 xl4">
+          <div className="user-list-item-card card">
+            <div className="card-image">
+              <img className="user-list-item-card-image" src={this.props.user.image} alt=""/>
+              <span className="card-title">{this.props.user.spotifyId}, {this.props.user.age}</span>
+              <button onClick={this.handleButtonClick.bind(this)} className={`btn-floating halfway-fab waves-effect waves-teal teal lighten-2 ${this.isFriendRequested() ? "disabled" : null}`}><i className="material-icons">person_add</i></button>
+            </div>
+            <div className="card-content">
+              <p>{this.props.user.about}</p>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    return null;
   }
 }
 
@@ -37,4 +48,4 @@ function mapStateToProps({auth}) {
   };
 }
 
-export default connect(mapStateToProps)(UserListItem);
+export default connect(mapStateToProps, actions)(UserListItem);
