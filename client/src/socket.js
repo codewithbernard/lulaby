76443c208ab9df597fa8d7a145bdc29cec96f9c8
Bernard Bado <bado.bernard94@gmail.com>
2018-02-05 16:46:05 +0100

    Using env variable for socket host

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,2 @@
 import io from 'socket.io-client';
-const host = process.env.PORT ? "https://lulaby.herokuapp.com" : 'http://localhost:5000';
-export default io(host);
+export default io(process.env.REACT_APP_SOCKET_HOST);
