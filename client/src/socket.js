8b343e85a40940b9727396cfc1c0e9af0389ee48
Bernard Bado <bado.bernard94@gmail.com>
2018-02-05 16:38:49 +0100

    Changing socket io host for prod

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,3 @@
 import io from 'socket.io-client';
-const host = process.env.PORT ? `https://localhost:${process.env.PORT`
-export default io(`http://localhost:${process.env.PORT || 5000}`);
+const host = process.env.PORT ? `https://localhost:${process.env.PORT}` : 'http://localhost:5000';
+export default io(host);
