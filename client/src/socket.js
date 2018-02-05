f4f806a03985814aef95f1ea419df828611b68eb
Bernard Bado <bado.bernard94@gmail.com>
2018-02-05 16:43:55 +0100

    Fixing host typo

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,3 @@
 import io from 'socket.io-client';
-const host = process.env.PORT ? "https://lulaby.herokuapp.com/" : 'http://localhost:5000';
+const host = process.env.PORT ? "https://lulaby.herokuapp.com" : 'http://localhost:5000';
 export default io(host);
