ce11e5e7a98910166f24c90ec03ebaa703415d4e
Bernard Bado <bado.bernard94@gmail.com>
2018-02-05 16:19:48 +0100

    Adding prod keys

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  spotifyClientId: 'd90e6ba0f5c74c4383673dafc63505e2',
-  spotifyClientSecret: 'a9c54101467e44088f19e575519a64ca',
-  mongoURI: 'mongodb://admin:12369874@ds249355.mlab.com:49355/lullaby-dev',
-  cookieKey: 'safasfasfsfa',
-  cloudinaryKey: '621818579124442',
-  cloudinarySecret: 'wt1Pif-0KUF-hTajrey5pqrk9uY'
-}
---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  cloudinaryKey: process.env.CLOUDINARY_KEY,
+  cloudinarySecret: process.env.CLOUDINARY_SECRET
+}
